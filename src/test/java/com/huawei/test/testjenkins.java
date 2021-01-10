3beaecfb056b43c8459be5f30ee0c7fa23f72c1c
scott <1402499019@qq.com>
2021-01-10 13:34:42 +0800

    增加testjenkins.java,删除test01等

---
+++ src/test/java/com/huawei/test/testjenkins.java
@@ -0,0 +1,12 @@
+package com.huawei.test;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class testjenkins {
+    private static Logger logger = LoggerFactory.getLogger(testjenkins.class);
+
+    public static void main(String[] args) {
+        logger.info("ceshi");
+    }
+}
